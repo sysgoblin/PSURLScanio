d7ba314ebcbd63b2192434938c4e925def465800
sysgoblin <cbaylissmk2@gmail.com>
2019-07-23 21:41:13 +0100

    initial

---
+++ Set-UrlScanConfig.ps1
@@ -0,0 +1,13 @@
+function Set-UrlScanConfig {
+    [CmdletBinding()]
+    param (
+        [string]$ApiKey
+    )
+
+    if (!(Test-Path -Path "$($env:AppData)\psurlscanio\urlscankey.json")) {
+        New-Item -Path "$($env:AppData)\psurlscanio\urlscankey.json" -Force
+    }
+
+    $apiKey = ConvertTo-SecureString $ApiKey -AsPlainText -Force | ConvertFrom-SecureString
+    $apiKey | Out-File "$($env:AppData)\psurlscanio\urlscankey.json" -Force
+}
