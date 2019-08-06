e0f52c8db557512273c4ca0a2ccc07ba59812a69
sysgoblin <cbaylissmk2@gmail.com>
2019-08-06 17:13:23 +0100

    added verbosity

--- public/Connect-UrlScanio.ps1
+++ public/Connect-UrlScanio.ps1
@@ -6,8 +6,10 @@ function Connect-UrlScanio {
 
     if (!(Test-Path -Path "$($env:AppData)\psurlscanio\urlscankey.json")) {
         New-Item -Path "$($env:AppData)\psurlscanio\urlscankey.json" -Force
+        Write-Verbos "Creating config file at $($env:AppData)\psurlscanio\urlscankey.json"
     }
 
     $apiKey = ConvertTo-SecureString $ApiKey -AsPlainText -Force | ConvertFrom-SecureString
     $apiKey | Out-File "$($env:AppData)\psurlscanio\urlscankey.json" -Force
+    Write-Verbose "Saved key as secure string to config file"
 }
