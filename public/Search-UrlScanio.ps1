974f30ab4e4ae157747010714ab7e9efa8b26528
sysgoblin <cbaylissmk2@gmail.com>
2019-08-06 18:15:29 +0100

    added help

--- public/Search-UrlScanio.ps1
+++ public/Search-UrlScanio.ps1
@@ -1,6 +1,52 @@
 function Search-UrlScanio {
+<#
+.SYNOPSIS
+Search urlscan.io
+
+.DESCRIPTION
+Search urlscan.io using the provided parameters or providing a filter using Elasticsearch Query String syntax
+(https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html)
+
+Params specified are combined to form a valid filter string.
+
+.PARAMETER Domain
+Domain to return results for (equivilent to page.domain)
+
+.PARAMETER IP
+Limit results or return those which are related to the IP provided
+
+.PARAMETER ASN
+Limit results or return those which are related to the ASN provided
+
+.PARAMETER ASNName
+Limit results or return those which are related to the ASN Name provided
+
+.PARAMETER Filename
+Limit results or return those which are related to the file name provided
+
+.PARAMETER Hash
+Limit results or return those which are related to the hash provided
+
+.PARAMETER Server
+Limit results or return those which are related to the server provided
+
+.PARAMETER Filter
+Filter string
+
+.PARAMETER Limit
+Number of results to return (default 100)
+
+.PARAMETER Raw
+Return results as raw json
+
+.EXAMPLE
+Search-UrlScanio -Domain google.com -Limit 10
+Returns last 10 scans completed on the domain google.com
+#>
+
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory = $true)]
         [string]$Domain,
         [string]$IP,
         [string]$ASN,
@@ -26,7 +72,7 @@ function Search-UrlScanio {
         # query builder
         $query = @()
         $PSBoundParameters.GetEnumerator() | % {
-            if (($_.Key -ne 'Limit') -and ($_.Key -ne 'Raw') -and ($_.Key -ne 'Debug')) {
+            if (($_.Key -ne 'Limit') -and ($_.Key -ne 'Raw')) {
                 $k = $_.Key.ToLower()
                 $v = $_.Value.ToLower()
 
@@ -50,7 +96,7 @@ function Search-UrlScanio {
                 [PSCustomObject]@{
                     TaskDate = $_.task.time
                     Submission = $_.task.method
-                    id = $_._id
+                    uuid = $_._id
                     URL = $_.page.url
                     ApiResult = $_.result
                     ResultPage = $_.result -replace '/api/v1'
@@ -60,6 +106,6 @@ function Search-UrlScanio {
     }
 
     end {
-            return $out
+        return $out
     }
 }
