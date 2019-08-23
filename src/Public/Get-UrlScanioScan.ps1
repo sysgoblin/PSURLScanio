37ab5f2387025db5d19c1a2ca6b5d818234a589b
sysgoblin <cbaylissmk2@gmail.com>
2019-08-23 19:24:01 +0100

    removed unrequired calls

---
+++ src/Public/Get-UrlScanioScan.ps1
@@ -0,0 +1,42 @@
+function Get-UrlScanioScan {
+<#
+.SYNOPSIS
+Get urlscan.io scan results
+
+.DESCRIPTION
+Get urlscan.io scan results for the provided uuid. Returns as converted json by default.
+
+.PARAMETER uuid
+UUID of scan to get details on
+
+.PARAMETER Raw
+Returns data as raw json
+
+.EXAMPLE
+Get-UrlScanioScan -uuid b14db0aa-013c-4aa9-ad5a-ec947a2278c7
+Get urlscan.io report for the scan with uuid b14db0aa-013c-4aa9-ad5a-ec947a2278c7
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+        Position = 0,
+        ValueFromPipelineByPropertyName)]
+        [ValidatePattern('[\d\w]{8}-[\d\w]{4}-[\d\w]{4}-[\d\w]{4}-[\d\w]{12}')]
+        [string]$uuid,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$Raw
+    )
+
+    process {
+        $request = Invoke-RestMethod -Uri "https://urlscan.io/api/v1/result/$uuid" -Headers $headers -ErrorAction:SilentlyContinue
+
+        if ($PSBoundParameters.Raw) {
+            $request = $request | ConvertTo-Json
+        }
+    }
+
+    end {
+        return $request
+    }
+}
